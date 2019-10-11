ea454ce19dc6565ebb4085df3461880ee61192c1
trista <781001063@qq.com>
2019-10-11 11:18:19 +0800

    初始版本V11,搭建大体框架

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    app
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<!-- 声明stylus语法 -->
+<style lang="stylus" rel="stylesheet/stylus">
+#app 
+  color red
+</style>
